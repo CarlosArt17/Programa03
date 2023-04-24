cd40ee1ad1a82718dbcba120a773b3066f3050a8
minio <zs20006730@estudiantes.uv.mx>
2023-04-23 19:32:49 -0500

    Subir documento

---
+++ src/main/java/org/uv/programa03/ClaseAHija.java
@@ -0,0 +1,14 @@
+package org.uv.programa03;
+
+/**
+ *
+ * @author minio
+ */
+public class ClaseAHija extends ClaseA{
+
+    @Override
+    public void msg() {
+        System.out.println("Hola en abstracto...");
+    }
+    
+}
